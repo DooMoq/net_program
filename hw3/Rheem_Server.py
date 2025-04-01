ec9bbd59c77f14bccace3b5dd9caef78e790848d
Doo_Moq <doomoq0215@gmail.com>
2025-04-01 16:59:59 +0900

    HW#

---
+++ hw3/Rheem_Server.py
@@ -0,0 +1,16 @@
+import socket
+s = socket.socket(socket.AF_INET, 
+ socket.SOCK_STREAM)
+s.bind(('', 9000))
+s.listen(2)
+while True:
+ client, addr = s.accept()
+ print('Connection from ', addr)
+ client.send(b'Hello ' + addr[0].encode())
+ # 학생의 이름을 수신한 후 출력
+ # 학생의 학번을 전송
+ msg = client.recv(1024)
+ print(msg.decode())
+ num = 20201522
+ client.send(num.to_bytes(8, 'little'))
+ client.close()
---
+++ hw3/Rheem_client.py
@@ -0,0 +1,13 @@
+import socket
+sock = socket.socket(socket.AF_INET, 
+ socket.SOCK_STREAM)
+addr = ('localhost', 9000)
+sock.connect(addr)
+msg = sock.recv(1024)
+print(msg.decode())
+# 본인의 이름을 문자열로 전송
+# 본인의 학번을 수신 후 출력
+sock.send(b'Rheem Seunghyun')
+num = sock.recv(1024)
+print(int.from_bytes(num, 'little'))
+sock.close()
